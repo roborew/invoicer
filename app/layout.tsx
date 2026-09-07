4d54146eeb8e72257105863b8d8832c1201956e8
Robin Andrew <robinrew@gmail.com>
2026-09-07 11:04:09 +0100

    Added routing and Layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '@/app/ui/global.css';
+
+
 import { inter } from '@/app/ui/fonts';
 import { roboto } from '@/app/ui/fonts';
 
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,10 +1,15 @@
 import { Inter } from 'next/font/google';
-import { Roboto } from 'next/font/google'
-
+import { Roboto } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
   display: 'swap',
-})
+});
+export const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+});
