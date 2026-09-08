aedc4884503408a6b673eababa27409c0c04b428
Robin Andrew <robinrew@gmail.com>
2026-09-08 12:37:47 +0100

    fxide the form errors for edit-invoice

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  {
+    settings: {
+      react: {
+        version: "19.2",
+      },
+    },
+  },
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
